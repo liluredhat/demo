ccc19d8b94b78ae16fdc19b6decc1e946cafc7e4
liluredhat <liluredhat@126.com>
2017-04-22 20:45:58 +0800

    测试代码

--- src/main/java/com/hnisi/basic/MavenPOMGeneratorTest.java
+++ src/main/java/com/hnisi/basic/MavenPOMGeneratorTest.java
@@ -12,10 +12,15 @@ public class MavenPOMGeneratorTest {
 		System.out.println("该目录下对象个数：" + tempList.length);
 
 		for (int i = 0; i < tempList.length; i++) {
-			
+
 			if (tempList[i].isFile()) {
 				String jarName = tempList[i].getName();
-				System.out.println("文     件：" + jarName);
+				//System.out.println("文     件：" + jarName);
+				String[] j = jarName.split("-");
+				if (j.length > 1) {
+					System.out.print("artifactId:"+ j[0] + " ");
+					System.out.println("version:" + j[1]);
+				}
 			}
 		}
 	}
