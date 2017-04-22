7bbcfa0145701268c9a4a9434eb306f79976d404
liluredhat <liluredhat@126.com>
2017-04-22 23:20:44 +0800

    maven

--- src/main/java/com/hnisi/basic/MavenPOMGeneratorTest.java
+++ src/main/java/com/hnisi/basic/MavenPOMGeneratorTest.java
@@ -1,25 +1,35 @@
 package com.hnisi.basic;
 
 import java.io.File;
-
+/**
+ * 
+ * @author lilu
+ *
+ */
 public class MavenPOMGeneratorTest {
 
 	public static void main(String[] args) {
-		String path = "D://hnisi/workspace/basicpt/WebRoot/WEB-INF/lib";
+		String path = "/Users/lilu/hnisi/temp";
 
 		File file = new File(path);
 		File[] tempList = file.listFiles();
 		System.out.println("该目录下对象个数：" + tempList.length);
 
 		for (int i = 0; i < tempList.length; i++) {
-
+			
 			if (tempList[i].isFile()) {
 				String jarName = tempList[i].getName();
 				//System.out.println("文     件：" + jarName);
-				String[] j = jarName.split("-");
-				if (j.length > 1) {
-					System.out.print("artifactId:"+ j[0] + " ");
-					System.out.println("version:" + j[1]);
+				if(jarName.endsWith(".jar")){
+					int beginIndex = jarName.lastIndexOf("-");
+					int endIndex = jarName.lastIndexOf(".jar");
+					String artifactId = jarName.substring(0, beginIndex);
+					String version = jarName.substring(beginIndex+1, endIndex);
+					System.out.println("<dependency>");
+					System.out.println("  <groupId>com.hnisi</groupId>");
+					System.out.println("  <artifactId>"+artifactId+"</artifactId>");
+					System.out.println("  <version>"+version+"</version>");
+					System.out.println("</dependency>");
 				}
 			}
 		}
