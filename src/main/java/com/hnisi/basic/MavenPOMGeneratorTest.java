21e39ca697e0557e4378fb1c65c4110e2febeb37
liluredhat <liluredhat@126.com>
2017-04-22 20:35:29 +0800

    maven

---
+++ src/main/java/com/hnisi/basic/MavenPOMGeneratorTest.java
@@ -0,0 +1,22 @@
+package com.hnisi.basic;
+
+import java.io.File;
+
+public class MavenPOMGeneratorTest {
+
+	public static void main(String[] args) {
+		String path = "D://hnisi/workspace/basicpt/WebRoot/WEB-INF/lib";
+
+		File file = new File(path);
+		File[] tempList = file.listFiles();
+		System.out.println("该目录下对象个数：" + tempList.length);
+
+		for (int i = 0; i < tempList.length; i++) {
+			
+			if (tempList[i].isFile()) {
+				String jarName = tempList[i].getName();
+				System.out.println("文     件：" + jarName);
+			}
+		}
+	}
+}
